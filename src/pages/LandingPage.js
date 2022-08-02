99b7c7d3fabbeb23efed7790aee5cf5c62e3a10b
dom <domalbano35@gmail.com>
2022-08-02 18:46:18 -0400

    imported main page components onto one main comp

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from './home'
+import About from './aboutMe'
+import Projects from './projects'
+
+import "../styles/landingpage.css";
+
+function LandingPage() {
+  return (
+    <div>
+      <div className='landingPage-container'>
+          {/* < Navbar /> */}
+          < Home />
+          < About />
+          < Projects />
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
