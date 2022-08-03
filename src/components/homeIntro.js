dcc31f212ba24da19b7428e2802448972dbda54f
dom <domalbano35@gmail.com>
2022-08-03 09:21:33 -0400

    changed app layed back to tab routes

---
+++ src/components/homeIntro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/homeintro.css'
+
+function HomeIntro() {
+  return (
+    <div className='homeIntroContainer' >
+        <h1 className='name'>Dominick Albano</h1>
+        <h2 className='title'>Full Stack Developer</h2>
+    </div>
+  )
+}
+
+export default HomeIntro
--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Navbar/>
-      <App />
+      < Navbar />
+      < App />
+      < Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
