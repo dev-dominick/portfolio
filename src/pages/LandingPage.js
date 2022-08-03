df4ebed9d50921c5b5265a38ce8483ed6683818e
dom <domalbano35@gmail.com>
2022-08-03 09:22:03 -0400

    not using component anymore at the moment

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,7 +9,6 @@ function LandingPage() {
   return (
     <div>
       <div className='landingPage-container'>
-          {/* < Navbar /> */}
           < Home />
           < About />
           < Projects />
