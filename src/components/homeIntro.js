f7e37aac16574659a83c4c7468b5410fdf0e0c22
dom <domalbano35@gmail.com>
2022-09-09 16:10:20 -0400

    made home component to render onto homepage

--- src/components/homeIntro.js
+++ src/components/homeIntro.js
@@ -5,9 +5,13 @@ import Socials from './socials'
 function HomeIntro() {
   return (
     <div className='homeIntroContainer' >
-      <h1 className='name'>Dominick Albano</h1>
-      <h2 className='title'>Full Stack Web Developer</h2>
-      <div className='socials'>
+
+      <div className='nameContainer'>
+        <h1 className='name'>Dominick Albano</h1>
+        <h2 className='title'>Full Stack Web Developer</h2>
+      </div>
+
+      <div className='socialsContainer'>
         < Socials />
       </div>
 
