c14c54eda8dd3fd797ceb33717ff53c676aa990e
dom <domalbano35@gmail.com>
2022-09-18 16:50:40 -0400

    media quiery changes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import "../styles/projects.css";
 import DisplayProjects from "../components/DisplayProjects";
-
+import Footer from '../components/footer';
 
 export default function Projects() {
   return (
     
     <div className='ProjectsPageStyles' id='project-section'>
 
-      <h1 className='pageHeader'>My Work</h1>
-      <p>Here are a few cool projects I have built!</p>
+      <h1 className='workHeader'>My Work</h1>
+      <p className='headerInfo'>Here are a few cool projects I have built!</p>
 
       
         < DisplayProjects />
       
+        < Footer />
 
     </div>
   );
