4843492257853e29d4085014470f0dae9d72fad5
dom <domalbano35@gmail.com>
2022-09-18 16:49:52 -0400

    added resume button, media quieries and styles

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import '../styles/aboutMe.css'
 // import Carousel from '../components/Carousel';
-
+import Footer from '../components/footer';
 export default function About() {
   return (
     <div className='AboutContainer' id='about-section'>
-      <main className="aboutme-header">
 
-        <h3 className="header">About Me</h3>
+      <h1 className="header">About Me</h1>
 
-        <p className="aboutMeText">
-          Full-stack web developer leveraging an array of transferable skills
-          from former career. These skills include leadership, a strong teamwork
-          ethic, and the ability to learn as you go. Known as an innovative
-          problem solver, passionate about developing apps, with a focus on
-          mobile-first design and development. Excited to learn new technologies
-          and implement them into future personal and open-source projects.
-          Recently earned a certificate in full stack development from the
-          University of Pennsylvania, with newly developed skills in JavaScript,
-          CSS, React.js, Express, and responsive web design. I have experience
-          with certain aspects of UX design by designing wireframes in order to
-          build the front-end of the app. I also have used Kanban boards,
-          similar to agile development, in many of my projects. Excited to learn
-          new technologies and implement them into future personal and
-          open-source projects.
-        </p>
-        
-      </main>
+      <div class='aboutIntro'>
+        <div className='textContainer'>
+
+          <p className="aboutMeText">
+            A Full-stack web developer with experience building custom software and web applications.
+            Passionate about building reusable and scalable code.  Looking foward to building and contributing to projects
+            with my knowledge of multiple programming technologies.
+          </p>
+
+          <a class='btnLink' href={require('../assets/DominickDevResume.pdf')} target='_blank' rel='noreferrer'>
+            <span className='resumeBtn'>Download Resume</span>
+          </a>
+        </div>
+
+        <div className='pictureContainer'>
+          <img className='aboutImg'
+            src={require('../assets/aboutMePic.jpg')}
+            alt='Laptop on Desk'></img>
+        </div>
+      </div>
+
+
+
+      < Footer />
 
       {/* < Carousel /> */}
     </div>
