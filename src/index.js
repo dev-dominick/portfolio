4501cb1dbc5410e8e26ee7c01c2ed50686fcb4e4
dom <domalbano35@gmail.com>
2022-09-18 16:47:09 -0400

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// routing
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import Navbar from './components/navbar';
-import Socials from './components/socials';
-import Footer from './components/footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,8 +12,6 @@ root.render(
     <BrowserRouter>
       < Navbar />
       < App />
-      {/* < Socials /> */}
-      < Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
