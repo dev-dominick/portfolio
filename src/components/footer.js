f09fc40fd8756d6640292cd7c206ce892fee13ab
dom <domalbano35@gmail.com>
2022-09-18 16:48:34 -0400

    changed element from h6 to p tag

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import '../styles/footer.css'
 
-
 export default function Footer() {
   return (
     <div className='footerContainer'>
-        <h6 className='footer'>
+        <p className='footer'>
         Dominick Albano © 2022
-        </h6>
+        </p>
     </div>
   )
 }
