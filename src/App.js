24150e9ff80179935f177e3abe5dbda0746cb736
dom <domalbano35@gmail.com>
2022-09-18 16:46:22 -0400

    removed unused imorts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import LandingPage from "./pages/LandingPage";
 import About from './pages/aboutMe'
 import Contact from './pages/contactMe'
 import Projects from './pages/projects'
-import Navbar from "./components/navbar";
-import Footer from "./components/socials";
 import Home from "./pages/home";
 import './App.css'
-
-//import the nav bar component here
 
 function App() {
   return (
     <div className="routesContainer">
-      {/* <Navbar/> */}
-
-
         <Routes>
           <Route path="/" element={< Home />} />
           <Route path="/about" element={< About />} />
